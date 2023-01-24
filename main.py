9b36b20789608a48f0b0dbab98c9d616e5d59583
arinze1020 <arinzeugwuanyi@gmail.com>
2023-01-24 19:44:06 +0100

    new update

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+software_names = [SoftwareName.CHROME.value]
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
+
+user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=10000)
+user_agent = user_agent_rotator.get_random_user_agent()
+chrome_options = Options()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+#chrome_options.add_argument('--proxy-server=%s' % random_ip)
+mobile_emulation = {
+    "deviceMetrics": {"width": 400, "height": 640, "pixelRatio": 3.0},
+    "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
+chrome_options.add_experimental_option(
+    "mobileEmulation", mobile_emulation)
+chrome_options.add_argument(f"user-agent={user_agent}")
+chrome_options.add_argument('--ignore-ssl-errors=yes')
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-gpu')
+chrome_prefs = {}
+
+chrome_options.experimental_options["prefs"] = chrome_prefs
+chrome_prefs["profile.default_content_settings"] = {"images": 2}
+driver = webdriver.Chrome(options=chrome_options)
+
+    # Do stuff with your driver
+driver.close()
+
+    
+
+
